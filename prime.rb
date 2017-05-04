9784d9bc1b5dbac98181e69d8d7145afc17e3e8e
Sarah Sweat <sarahsweat@gmail.com>
2017-05-04 17:57:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+
+  flag = true
+  if num > 1
+    for x in 2..num-1
+        if num % x == 0
+            flag = false
+        end
+    end
+  else
+    flag = false
+  end
+    return flag
+end
